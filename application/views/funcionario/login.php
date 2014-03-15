d0064a5a14a951e20ca95b7a92215c490c425eb7
Sérgio <sergiobh@gmail.com>
2014-03-15 18:28:39 -0300

    Inserido o validador de campos Validate do jquery-1.11.0.js e tentando configurar a exibição das mensagens por Tooltip

--- application/views/funcionario/login.php
+++ application/views/funcionario/login.php
@@ -1,36 +1,70 @@
-<form class="form-signin" novalidate role="form">
+<form class="form-signin" role="form" >
 	<h2 class="form-signin-heading">Controle de Acesso</h2>
 	<div class="form-group">
-		<input type='text' class="form-control" placeholder="Cpf" required autofocus maxlength='11' id='Cpf' name='cpf' alt="${g.message(code:'codigo_mensagem')}" />
+		<input type='number' class="form-control" placeholder="Cpf" autofocus maxlength='11' id='Cpf' name='cpf' />
 	</div>
 	<div class="form-group">
-		<input type='password' class="form-control" placeholder="Senha" required maxlength='10' id='Senha' name='senha' /> <label class="checkbox">
+		<input type='password' class="form-control" placeholder="Senha" maxlength='10' id='Senha' name='senha' />
 	</div>
-	<button class="btn btn-lg btn-primary btn-block botao_submit">Logar</button>
+	<button class="btn btn-lg btn-success btn-block botao_submit">Logar</button>
 	
 </form>
 <script type="text/javascript">
 $(document).ready(function(){
+	var validator = $(".form-signin").validate({
+		debug: true,
+		//onsubmit: false,
+		rules: ( "add", {
+			cpf: {
+				required: true,
+				minlength: 11
+			},
+			senha: {
+				required: true,
+				maxlength: 10,
+				minlength: 6
+			}
+		}),
+		messages: {
+			cpf:{
+                required: "Campo obrigatório",
+                minlength: "Digite um Cpf válido",
+                number: "Digite um Cpf válido"
+            },
+			senha: {
+                required: "Campo obrigatório",
+                maxlength: "Digite no máximo 10 caracteres",
+                minlength: "Digite no mínimo 6 caracteres"
+			}
+		}
+	});
 
-  $('.form-signin').submit(function(){
-  
-    var MSGContato = '<div class="bloco_msg"><b>I.</b> Não revelar fora do âmbito profissional estado ou informação de qualquer natureza de que tenha conhecimento por força de minhas atribuições, salvo em decorrência de decisão competente na esfera legal ou judicial, bem como de autoridade superior;</div>';
-    MSGContato += '<br /><div class="bloco_msg"><b>II.</b> Utilizar os dados do sistema informatizado de acesso restrito e manter a necessária cautela quando da exibição de dados em tela, impressora ou na gravação em meios eletrônicos, a fim de evitar que deles venham a tomar ciência pessoas não autorizadas;</div>';
-    MSGContato += '<br /><div class="bloco_msg"><b>III.</b> Não me ausentar da estação de trabalho sem encerrar a sessão de uso do sistema, garantindo assim a impossibilidade de acesso indevido por terceiros;</div>';
-    MSGContato += '<br /><div class="bloco_msg"><b>IV.</b> Não revelar minha senha de acesso ao(s) sistema(s) a ninguém e tomar o máximo de cuidado para que ela permaneça somente de meu conhecimento;</div>';
-    MSGContato += '<br /><div class="bloco_msg">Declaro, nesta data, ter ciência e estar de acordo com os procedimentos acima descritos, comprometendo-me a respeitá-los e cumpri-los plena e integralmente, além de manter sempre verdadeiros os dados de instituição e de minha área de competência.</div>';
+	$(".form-signin").submit(function () {
+	    if (validator.form()) {
+	        termoResponsabilidade();
+	    }
+
+        return false;
+	});
+
+	//$(".form-signin").submit(function(){
+	function termoResponsabilidade(){
+		var MSGContato = '<div class="bloco_msg"><b>I.</b> Não revelar fora do âmbito profissional estado ou informação de qualquer natureza de que tenha conhecimento por força de minhas atribuições, salvo em decorrência de decisão competente na esfera legal ou judicial, bem como de autoridade superior;</div>';
+	    MSGContato += '<br /><div class="bloco_msg"><b>II.</b> Utilizar os dados do sistema informatizado de acesso restrito e manter a necessária cautela quando da exibição de dados em tela, impressora ou na gravação em meios eletrônicos, a fim de evitar que deles venham a tomar ciência pessoas não autorizadas;</div>';
+	    MSGContato += '<br /><div class="bloco_msg"><b>III.</b> Não me ausentar da estação de trabalho sem encerrar a sessão de uso do sistema, garantindo assim a impossibilidade de acesso indevido por terceiros;</div>';
+	    MSGContato += '<br /><div class="bloco_msg"><b>IV.</b> Não revelar minha senha de acesso ao(s) sistema(s) a ninguém e tomar o máximo de cuidado para que ela permaneça somente de meu conhecimento;</div>';
+	    MSGContato += '<br /><div class="bloco_msg">Declaro, nesta data, ter ciência e estar de acordo com os procedimentos acima descritos, comprometendo-me a respeitá-los e cumpri-los plena e integralmente, além de manter sempre verdadeiros os dados de instituição e de minha área de competência.</div>';
 
 
-    jConfirm(MSGContato, 'TERMO DE RESPONSABILIDADE', function(r) {
-      if(r){
-        logar();
-      }
-    });
-
-	// retorno para que não ocorra o submit do form
-    return false;
-
-  });
+	    jConfirm(MSGContato, 'TERMO DE RESPONSABILIDADE', function(r) {
+	      if(r){
+	        logar();
+	      }
+	    });
+	
+		// retorno para que não ocorra o submit do form
+	    return false;
+  	};
 
 
   function logar(){
--- application/views/libraries/header.php
+++ application/views/libraries/header.php
@@ -8,7 +8,7 @@
 		<meta http-equiv="content-language" content="pt-BR" />		
         <link rel="stylesheet" href="<?php echo BASE_URL;?>/web/css/global.php" type="text/css" media="screen" />
         <?php /*<link rel="stylesheet" href="<?php echo BASE_URL;?>/web/css/custom_layout.css" type="text/css" media="screen" /> */?>
-		<script type="text/javascript" src="<?php echo BASE_URL;?>/web/js/jquery/jquery.js"></script>
+		<script type="text/javascript" src="<?php echo BASE_URL;?>/web/js/jquery/jquery-1.11.0.js"></script>
 		<script type="text/javascript" src="<?php echo BASE_URL;?>/web/js/jquery/jquery.blockUI.js"></script>
 		<script type="text/javascript" src="<?php echo BASE_URL;?>/web/js/jquery/jquery.tablesorter.min.js"></script>
 		<script type="text/javascript" src="<?php echo BASE_URL;?>/web/js/global.php"></script>
@@ -17,16 +17,19 @@
 		==========================================================-->
 		<link href="<?php echo BASE_URL; ?>/public/css/bootstrap.css" rel="stylesheet">
 		<link href="<?php echo BASE_URL; ?>/public/css/font-awesome.css" rel="stylesheet">
+		<link href="<?php echo BASE_URL; ?>/public/css/tooltipster.css" rel="stylesheet">		
 
 		<?php /* Minha customização de tela */?>
 		<link href="<?php echo BASE_URL; ?>/public/css/custom.css" rel="stylesheet">
 		
 		<!--Le JavaScript
 		==========================================================-->
-		<?php /*
-		<script src="<?php echo BASE_URL; ?>/public/js/jquery.js"></script>
-		*/ ?>
-		<script src="<?php echo BASE_URL; ?>/public/js/bootstrap.js"></script>		
+		<script src="<?php echo BASE_URL; ?>/public/js/bootstrap.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/js/jquery.validate.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/js/additional-methods.js"></script>
+
+		<script src="<?php echo BASE_URL; ?>/public/js/jquery.tooltipster.js"></script>
+				
 		
 		<?php
 			if(isset($Script)){
