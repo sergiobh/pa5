5970cfe149c698e809251cbe7d6d263029575811
Sérgio <sergiobh@gmail.com>
2014-03-15 18:31:17 -0300

    Inserido no rodape o nome do Humberto

--- application/views/body/footer.php
+++ application/views/body/footer.php
@@ -1,4 +1,4 @@
 <div data-role="footer" align="center" data-theme="d" >
 	Software de Gestão de Hotelaria Hospitalar (SGHH)<br>
-	Enrique Bonifácio - Fábio Mello - Jorge Pimenta - Juliana - Sérgio Macedo - Weslei Nunes<br>
+	Enrique Bonifácio - Fábio Mello - Humberto Paulus - Jorge Pimenta - Juliana - Sérgio Macedo - Weslei Nunes<br>
 	UNA-BH 1sem/2014 - Todos os direitos reservados.
